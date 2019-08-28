4b9985b51740d501b973a87520463d3a18ff7570
sunil issac <issacsun10@gmail.com>
2019-08-28 17:28:31 +1000

    Specflow

---
+++ Firstautomation/FeatureFiled/TMFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: TMFeature
+	As a turnup portal i would like to manage time and
+	material record in single page
+
+@mtm @automate
+Scenario: Turn up admin able to create time and material records
+	Given I have loggedin to the turn up portal
+	And I have navigate to time and material page
+	Then I should be able to create time and material records
---
+++ Firstautomation/Hookup/TMFeatureSteps.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Firstautomation.Hookup
+{
+    [Binding]
+    public class TMFeatureSteps
+    {
+        IWebDriver driver;
+        [Given(@"I have loggedin to the turn up portal")]
+        public void GivenIHaveLoggedinToTheTurnUpPortal()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+            driver.Manage().Window.Maximize();
+            LoginPage logininstance = new LoginPage(driver);
+            logininstance.LoginSucess();
+        }
+        
+        [Given(@"I have navigate to time and material page")]
+        public void GivenIHaveNavigateToTimeAndMaterialPage()
+        {
+            EnterValues entervaluesinstance = new EnterValues(driver);
+            entervaluesinstance.details();
+        }
+        
+        [Then(@"I should be able to create time and material records")]
+        public void ThenIShouldBeAbleToCreateTimeAndMaterialRecords()
+        {
+            driver.Quit();
+        }
+    }
+}
